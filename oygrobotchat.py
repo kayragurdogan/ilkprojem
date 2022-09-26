57ced0dd62d051f88e94ffe06690ba100ec2c10a
kayragurdogan <kayragurdogan050505@gmail.com>
2022-09-26 19:25:36 +0300

    oygrobotchat

---
+++ oygrobotchat.py
@@ -0,0 +1,18 @@
+import random
+
+print("hoşeldin!")
+oynamak_istiyor_musun=input("Oynamak ister misin")
+
+while oynamak_istiyor_musun == "Evet":
+    cevap = random.randint(1, 100)
+    tahmin = int(input("1 ile 100 arasında bir sayı bul!"))
+
+    while tahmin != cevap:
+        if tahmin > cevap:
+            print("Yanlış, bir dahakine daha küçük bir sayı tahmin et!")
+        if tahmin < cevap:
+            print("daha büyük bir sayı bul!")
+        tahmin = int(input("1 ile 100 arasındaki sayıyı tahmin et!"))
+
+
+    oynamak_istiyor_musun = input("yeni oyun? 'Evet', 'Hayır' \n")
